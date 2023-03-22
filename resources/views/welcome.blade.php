f61aed5f9478d74fbb0955176663fd9bee63fff2
Dobriyan Petkov <dobriyanp@yahoo.com>
2023-03-22 16:20:54 +0000

    First commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+    </head>
+    <body class="antialiased">
+        <h2>Hello World 12</h2>
+    </body>
+</html>
