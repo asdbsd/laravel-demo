a490815637313afbd583129dc834f8e2370cc69f
Dobriyan Petkov <dobriyanp@yahoo.com>
2023-03-27 16:33:01 +0100

    replaced posts querie with controller actions and included. Replaced category search with query search and removed category routing

--- routes/web.php
+++ routes/web.php
@@ -20,16 +20,15 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', [PostController::class, 'index'])->name('home');
-
 Route::get('/posts/{post}', [PostController::class, 'show']);
 
-Route::get('/categories/{category}', function(Category $category) {
-    return view('posts', [
-        'posts' =>  $category->posts,
-        'currentCategory'=> $category,
-        'categories' => Category::all()
-    ]);
-});
+// Route::get('/categories/{category}', function(Category $category) {
+//     return view('posts', [
+//         'posts' =>  $category->posts,
+//         'currentCategory'=> $category,
+//         'categories' => Category::all()
+//     ]);
+// });
 
 Route::get('/authors/{author}', function(User $author) {
     return view('posts', ['posts' => $author->posts]);
